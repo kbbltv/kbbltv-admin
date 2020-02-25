9d1de6aa456b9028db590a59d7541e1beec7a380
Cristian Schonmeier <criis.schon@gmail.com>
2020-02-25 13:55:32 -0300

    Search library component

--- src/app/shared/components/library/library.component.ts
+++ src/app/shared/components/library/library.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MediaService } from '../../services/media/media.service';
+import { Media } from '../../models/media/media';
 
 @Component({
   selector: 'app-library',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
+  search = '';
+  mediaList: Media[] = [];
+  @Output() mediaClicked = new EventEmitter();
+
+  constructor(
+    private readonly mediaService: MediaService
+  ) { }
 
   ngOnInit() {
+   this.searchMedia();
+  }
+
+  clickMedia(media) {
+    this.mediaClicked.emit(media);
+  }
+
+  searchMedia() {
+    let query = { page: 1, search: this.search };
+    console.log(query);
+    this.mediaService.search(query)
+    .then(mediaList => {
+      this.mediaList = mediaList.docs;
+    })
   }
 
 }
--- src/app/shared/components/library/library.module.ts
+++ src/app/shared/components/library/library.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibraryComponent } from './library.component';
-
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
   declarations: [LibraryComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatIconModule
   ],
   exports: [
     LibraryComponent
